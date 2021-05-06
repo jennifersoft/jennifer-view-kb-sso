8ae9f5c8d702c3d259b883f7309c5f3ca0ccf14d
alvin <seogi777@gmail.com>
2021-05-06 16:11:03 +0900

    어댑터 및 인증 API 개발 완료

--- src/main/kotlin/com/aries/kb/api/KbApiController.kt
+++ src/main/kotlin/com/aries/kb/api/KbApiController.kt
@@ -1,22 +1,32 @@
 package com.aries.kb.api
 
 import com.aries.extension.starter.PluginController
+import com.aries.extension.util.PropertyUtil
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import com.aries.kb.util.SelfExpiringHashMap
+import com.aries.kb.util.AES
 
 @RestController
 class KbApiController @Autowired constructor() : PluginController() {
     companion object {
+        val AUTH_KEYS = SelfExpiringHashMap<String, String>()
     }
 
     @GetMapping(value = [ "/kbapi/authkey" ])
-    fun createAuthKey(): ResponseEntity<ResponseData> {
-        return ResponseEntity(ResponseData(true), HttpStatus.OK)
-    }
+    fun createAuthKey(
+        @RequestParam(required = true) user_id: String,
+        @RequestParam(required = true) device_id: String
+    ): ResponseEntity<String> {
+        val key = user_id + device_id
+        val output = AES.encrypt(key,
+            PropertyUtil.getValue("kb_plugin", "KB_PASSWORD_SALT", "jennifer5"))
 
-    data class ResponseData(
-        var result: Boolean = false
-    )
+        AUTH_KEYS.put(key, output,
+            PropertyUtil.getValue("kb_plugin", "KB_VALIDATE_TIMEOUT", "3000").toLong())
+
+        return ResponseEntity(output, HttpStatus.OK)
+    }
 }
--- src/main/kotlin/com/aries/kb/login/KbLoginAdapter.kt
+++ src/main/kotlin/com/aries/kb/login/KbLoginAdapter.kt
@@ -4,6 +4,7 @@ import com.aries.extension.data.UserData
 import com.aries.extension.handler.SSOLoginHandler
 import com.aries.extension.util.LogUtil
 import com.aries.extension.util.PropertyUtil
+import com.aries.kb.api.KbApiController
 import com.aries.kb.util.OAMInsensitiveMap
 import com.aries.kb.util.OAMUtil
 import javax.servlet.http.HttpServletRequest
@@ -13,17 +14,28 @@ class KbLoginAdapter : SSOLoginHandler {
         val headerMap = OAMInsensitiveMap()
         OAMUtil.httpHeaderToMap(request, headerMap)
 
-        val sso_user_id: String = headerMap.get(PropertyUtil.getValue("kb_login", "KB_HEADER_KEY", "KB_USER_ID"))
-        val sso_user_password = PropertyUtil.getValue("kb_login", "KB_USER_PASSWORD", "")
+        val userId: String = headerMap.get("KB_USER_ID")
+        val deviceId: String = headerMap.get("KB_DEVICE_ID")
+        val authKey: String = headerMap.get("KB_AUTH_KEY")
 
-        println("header key : " + PropertyUtil.getValue("kb_login", "KB_HEADER_KEY", "KB_USER_ID"))
-        println("preHandle check: $sso_user_id ($sso_user_password)")
-
-        if (sso_user_id == null || sso_user_id.trim().isEmpty()) {
-            LogUtil.error("sso_user_id not found")
+        if (userId == null || deviceId == null || authKey == null) {
+            LogUtil.error("HTTP request attribute value required for authentication does not exist")
             return null
         }
 
-        return UserData(sso_user_id, sso_user_password)
+        val cachedAuthKey = KbApiController.AUTH_KEYS[userId + deviceId]
+
+        println("HTTP Request Headers: $userId, $deviceId, $authKey")
+        println("Cached Key: $cachedAuthKey")
+
+        if (authKey != cachedAuthKey) {
+            LogUtil.error("The authentication key is not valid")
+            return null
+        }
+
+        return UserData(
+            PropertyUtil.getValue("kb_login", "KB_JENNIFER_ID", "guest"),
+            PropertyUtil.getValue("kb_login", "KB_JENNIFER_PASSWORD", "guest")
+        )
     }
 }
